275cc5e142388d71acbcbcfeaf687a7e32f2d0cb
gunerhanale <gunerhanale@gmail.com>
2021-03-09 23:01:48 +0100

    realized managing transaction while working in SpringBoot

---
+++ src/main/java/com/portfolio/springboot/managingtransactions/ManagingTransactionsApplication.java
@@ -0,0 +1,14 @@
+package com.portfolio.springboot.managingtransactions;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingTransactionsApplication.class, args);
+    }
+
+}
